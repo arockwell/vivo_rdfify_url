8b601bd2bf23860e3101146eb1f022ab7de1ac7b
Alex Rockwell <alexhr@ufl.edu>
2011-04-07 12:42:52 -0400

    Updated to use n3 instead of xml+rdf

--- rdfify_url.js
+++ rdfify_url.js
@@ -1,1 +1,1 @@
-javascript:loc=location.href;re=/(display|individual)\/(.+)/;location='http://'+location.host+'/individual/'+loc.match(re)[2]+'/'+loc.match(re)[2]+'.rdf'
+javascript:loc=location.href;re=/(display|individual)\/(.+)/;location='http://'+location.host+'/individual/'+loc.match(re)[2]+'/'+loc.match(re)[2]+'.n3'
